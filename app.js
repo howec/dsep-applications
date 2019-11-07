563940df77b28bbb21537622a6db0a5f2a5fcce7
Howe <howe.cui@berkeley.edu>
2019-11-06 20:07:15 -0800

    Accessing Spreadsheets test

---
+++ app.js
@@ -0,0 +1,32 @@
+//"" -- indicates figures of speech or variable names
+//'' -- strings
+
+/*
+"Require" the express module and bind it to a variable named "express" and instantiate it
+*/
+const express = require('express');
+const app = express();
+
+
+/*
+Create a preconfigured requestListener function with the express object.
+This will be executed everytime the "server" gets a request.
+Set up socket.io to listen on the same port as whatever the server was configured on.
+*/
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+
+
+const port = process.env.PORT || 3000
+server.listen(port, function() {
+    console.log("Listening on port", port); //similar to pythonic f-string
+	}
+);
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/Webpages/index.html');
+});
